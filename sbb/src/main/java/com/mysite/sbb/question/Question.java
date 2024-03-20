1bc28ae21687e886ed29ae21d7bc6200d4e3a98b
bong9C <kathy1221@naver.com>
2024-03-20 22:05:00 +0900

    검색창 만들기

--- sbb/src/main/java/com/mysite/sbb/question/Question.java
+++ sbb/src/main/java/com/mysite/sbb/question/Question.java
@@ -2,6 +2,7 @@ package com.mysite.sbb.question;
 
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Set;
 
 import com.mysite.sbb.answer.Answer;
 import com.mysite.sbb.user.SiteUser;
@@ -12,6 +13,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
@@ -37,4 +39,10 @@ public class Question {
     
     @ManyToOne //  사용자 한명이 여러 질문을 작성 할 수도 있기 때문에 
     private SiteUser author;
+    private LocalDateTime modifyDate;
+    
+    @ManyToMany
+    Set<SiteUser> voter;
+
+
 }
